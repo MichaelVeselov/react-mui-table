5d7ef60b4a0d1e92ce540d1afe2887ac70bb7dce
Michael Veselov <michael.veselov@gmail.com>
2023-03-15 10:50:59 +0300

    initial commit - add Basic, DataGrid and Dense tables

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Table MUI Demo App
+
+## Diverse MUI Table Components
+
+### Content
+
+- Basic table
+- Datagrid table
+- Dense table
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import { BasicTable } from './components/BasicTable';
+import { DataGridTable } from './components/DataGridTable';
+import { DenseTable } from './components/DenseTable';
+
+function App() {
+  return (
+    <>
+      {/* <BasicTable /> */}
+      <DataGridTable />
+      {/* <DenseTable /> */}
+    </>
+  );
+}
+
+export default App;
